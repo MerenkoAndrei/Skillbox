1d1dcaabd896cb13a929e3241ebf8965ffc4886b
AndreiMerenko <merenko2016@mail.ru>
2023-11-05 19:46:36 +0300

    Задание выполнено

--- Primitives/src/IntOrInteger/Main.java
+++ Primitives/src/IntOrInteger/Main.java
@@ -1,3 +1,5 @@
+package IntOrInteger;
+
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
@@ -7,6 +9,13 @@ public class Main {
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
-
+        for (int i = 0; i < 65536; i++) {
+            char c = (char) i;
+            if (c == 'Ё' || c == 'ё') {
+                System.out.println(i + " - " + c);
+            } else if (c >= 'А' && c <= 'я') {
+                System.out.println(i + " - " + c);
+            }
+        }
     }
 }
