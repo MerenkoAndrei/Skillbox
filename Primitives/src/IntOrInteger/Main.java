e5fc7b0e5d41f7de8493e66f72bc88eb827ea417
AndreiMerenko <merenko2016@mail.ru>
2023-11-06 18:28:04 +0300

    Изменения

--- Primitives/src/IntOrInteger/Main.java
+++ Primitives/src/IntOrInteger/Main.java
@@ -12,9 +12,9 @@ public class Main {
         for (int i = 0; i < 65536; i++) {
             char c = (char) i;
             if (c == 'Ё' || c == 'ё') {
-                System.out.println(i + " - " + c);
+                System.out.println(i + "  -  " + c);
             } else if (c >= 'А' && c <= 'я') {
-                System.out.println(i + " - " + c);
+                System.out.println(i + "  -  " + c);
             }
         }
     }
